b1bb84416d9be437b95868b569102f0c57e969d2
LeoNefesch <leonid.negaschev@gmail.com>
2024-10-26 10:20:09 +0500

    26.10.2024 fix code according to technical task

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('users/', UserList.as_view(), name='user-list'),
-    path('users/<pk>/', UserDetails.as_view(), name='user-detail'),
     path('profile/', profile_view, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,28 +1,12 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import generics
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .serializers import UserSerializer
 
-# class UserList(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def get(self, request):
-#         users = User.objects.all()
-#         serializer = UserSerializer(users, many=True)
-#         return Response(serializer.data)
-
 
 class UserList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetails(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
